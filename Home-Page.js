3950d33d9a0eda6fcba3e2a25ddfcd6287e41c9a
Srushti Sawant <98805433+SrushtiSawant15@users.noreply.github.com>
2022-11-25 14:18:02 +0530

    Update Home-Page.js

--- Home-Page.js
+++ Home-Page.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-// Components
+
 import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard-Component";
 import HeroCarousel from "../components/HeroCarousel/HeroCarousel-Component";
 import PosterSlider from "../components/PosterSlider/PosterSlider-Component";
+ 
 
-// Layout HOC
 import DefaultLayoutHoc from "../layout/Default-layout";
 
 const HomePage = () => {
